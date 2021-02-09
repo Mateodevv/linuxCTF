173e6f7196e3e7dac3212a75dc83ee4f8bbaad0d
Mateodevv <74459761+Mateodevv@users.noreply.github.com>
2021-02-09 13:35:01 +0100

    Update challenge7.py

--- script/challenge7.py
+++ script/challenge7.py
@@ -2,12 +2,12 @@ import os
 
 for root, dirs, files in os.walk("/home/ubuntu/challenge7"):
     for filename in files:
-        if filename is "challenge7":
+        if filename is "challenge7.pw":
             fileA = filename
 
 for root, dirs, files in os.walk("/home/ubuntu/challenge7.1"):
     for filename in files:
-        if filename is "challenge7":
+        if filename is "challenge7.pw":
             fileB = filename
 
 if fileA is fileB:
