d4ff57a719faf3ceedaebc763a997e5d9a4050b4
maikwuerth <maikwuerth@users.noreply.github.com>
2021-02-15 17:37:44 +0100

    Update challenge10.sh
    update name of output file

--- helper/challenge10.sh
+++ helper/challenge10.sh
@@ -2,14 +2,14 @@
 
 cd /home/ubuntu/
 
-touch challenge10
+touch padawan_2021
 # line num of flag
 rand=$((100+$RANDOM%900))
 for i in {1..1000}; do
 	# flag
 	if [[ $i == $rand ]]; then
-		echo "flag_gute_arbeit_padawan" >> challenge10
+		echo "flag_gute_arbeit_padawan" >> padawan_2021
 	fi
 	# random 8 byte int
-	echo `od -vAn -N8 -tu8 < /dev/urandom` >> challenge10
+	echo `od -vAn -N8 -tu8 < /dev/urandom` >> padawan_2021
 done
