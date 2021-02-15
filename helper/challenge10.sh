0685937180091c102d90f529ef19ca239fad8179
maikwuerth <maikwuerth@users.noreply.github.com>
2021-02-15 17:36:45 +0100

    Update challenge10.sh
    flag update

--- helper/challenge10.sh
+++ helper/challenge10.sh
@@ -8,7 +8,7 @@ rand=$((100+$RANDOM%900))
 for i in {1..1000}; do
 	# flag
 	if [[ $i == $rand ]]; then
-		echo "flag-you-found-waldo" >> challenge10
+		echo "flag_gute_arbeit_padawan" >> challenge10
 	fi
 	# random 8 byte int
 	echo `od -vAn -N8 -tu8 < /dev/urandom` >> challenge10
